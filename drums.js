c63a05c3dbdf61977fddb061771a7f93894c8bdf
nicoskia <zeldawg@gmail.com>
2017-01-24 14:14:37 -0500

    created drum pad

---
+++ drums.js
@@ -0,0 +1,46 @@
+var boom = new Audio('sounds/boom.wav');
+var clap = new Audio("sounds/clap.wav");
+var hihat = new Audio("sounds/hihat.wav");
+var kick = new Audio("sounds/kick.wav");
+var openhat = new Audio("sounds/openhat.wav");
+var ride = new Audio("sounds/ride.wav");
+var snare = new Audio("sounds/snare.wav");
+var tink = new Audio("sounds/tink.wav");
+var tom = new Audio("sounds/tom.wav");
+
+function playBoom()
+  {
+    boom.play();
+  }
+function playClap()
+  {
+    clap.play();
+  }
+function playHihat()
+  {
+    hihat.play();
+  }
+function playKick()
+  {
+    kick.play();
+  }
+function playOpenhat()
+  {
+    openhat.play();
+  }
+function playRide()
+  {
+    ride.play();
+  }
+function playSnare()
+  {
+    snare.play();
+  }
+function playTink()
+  {
+    tink.play();
+  }
+function playTom()
+  {
+    tom.play();
+  }
